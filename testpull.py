dbac6528866bdb7c1a8d7c261981bad16af7b827
Sean <seann.so@hotmail.com>
2021-05-12 00:23:54 +1000

    add test script that outputs abstracts from up to 1000 results

--- testpull.py
+++ testpull.py
@@ -1,33 +1,16 @@
 from metapub import PubMedFetcher
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import json
 # import spacy
 # import os
 
-search_terms = input("Input search term:")
-=======
-import spacy
-
 search_terms = input("Input search terms:")
->>>>>>> Stashed changes
-=======
-import spacy
-
-search_terms = input("Input search terms:")
->>>>>>> Stashed changes
 fetch = PubMedFetcher()
 pmids = fetch.pmids_for_query(search_terms, retmax=1000)
 
 abstracts = {}
 for pmid in pmids:
     abstracts[pmid] = fetch.article_by_pmid(pmid).abstract
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 
 with open(search_terms.replace(" ", "_") + ".log", "w") as f:
     f.write(json.dumps(abstracts))
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
